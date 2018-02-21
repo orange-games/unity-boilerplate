1ae611154e8340bc7033cb423ae76eee6869e49b
AArends <a.arends@orangegames.com>
2018-02-21 10:22:53 +0100

    Project commit / Unity packages

---
+++ Unity_WebGL_Template/Assets/Editor/WebGLBuilder.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+public class WebGLBuilder {
+    static void build()
+    {
+   
+        // Place all your scenes here
+        string[] scenes = new string[EditorBuildSettings.scenes.Length-1];      
+
+        for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+        {
+            scenes[i] = EditorBuildSettings.scenes[i].path;
+        }
+
+        string pathToDeploy = "builds/WebGLversion/";
+
+        BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.WebGL, BuildOptions.None);
+    }
+}
